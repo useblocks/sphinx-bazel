bf9ba21f412c4d4a6aa80951c9f267f5bbbd5918
Daniel Woste <daniel.woste@useblocks.com>
2020-04-28 08:44:29 +0200

    Updates pip on travis

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@
 import os
 from setuptools import setup, find_packages
 
-requires = ['Sphinx']
+requires = ['Sphinx<2;python_version<"3.5"',
+            'Sphinx<3;python_version<"3.6"',
+            'Sphinx;python_version>="3.6"']
 
 setup(
     name='sphinx-bazel',
